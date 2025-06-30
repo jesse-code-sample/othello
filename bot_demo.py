474c3c4e7ce59a8434c73ddb1ec729de73885987
jesse-code-sample <yuan.jesse@gmail.com>
2025-06-30 01:58:53 -0400

    Change bot demo to use depth 4.

--- bot_demo.py
+++ bot_demo.py
@@ -2,8 +2,8 @@ from bot import CornersBot, GreedyBot, play_game
 
 if __name__ == "__main__":
     # Demo of the CornersBot (white) beating the GreedyBot (black)
-    greedy_bot = GreedyBot(depth=3)
-    corners_bot = CornersBot(depth=3)
+    greedy_bot = GreedyBot(depth=4)
+    corners_bot = CornersBot(depth=4)
     black_pieces, white_pieces = play_game(black_bot=greedy_bot,
                                            white_bot=corners_bot,
                                            print_moves=True)
